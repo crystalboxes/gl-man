25f730e6ecdf364f4baeea7ae93e52bc54ea1fb9
Oleg Pustovit <crystalboxesgfx@gmail.com>
2020-07-10 15:41:57 +0300

    exported API update

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,26 @@ import {
   setViewport,
   isContextInitialized,
 } from './gl/context'
-import { getRgba, getRandomColor } from './common/color'
+import {
+  getRgba,
+  getRandomColor,
+  setColor,
+  getAlpha,
+  getColor,
+} from './common/color'
 import { Shader, createShader } from './gl/shader'
 import { GraphicsBuffer, GraphicsBufferType } from './gl/graphics-buffer'
 import { Framebuffer } from './gl/framebuffer'
-import { draw } from './gl/draw'
+import { draw, TRIANGLE_STRIP, TRIANGLES } from './gl/draw'
+import { beginShape, endShape, vertex } from './drawing/shape'
+import { pushMatrix, popMatrix, translate, scale, rotate } from './common/transform'
+
+
+
+
+
+
+
 
 const Graphics = {
   clearColor,
@@ -40,7 +55,63 @@ const Graphics = {
   Shader,
   getRandomColor,
 
+  getColor,
+  setColor,
+  getAlpha,
+
+  beginShape,
+  endShape,
+  vertex,
+
+  pushMatrix,
+  popMatrix,
+  translate,
+  scale,
+  rotate,
+
+  TRIANGLE_STRIP,
+  TRIANGLES,
   FLOAT,
 }
 
 export default Graphics
+export {
+  clearColor,
+  createShader,
+  draw,
+  drawCircle,
+  drawRect,
+  Framebuffer,
+  getCurrentFramebuffer,
+  getGL,
+  getRgba,
+  getHeight,
+  getWidth,
+  GraphicsBuffer,
+  GraphicsBufferType,
+  isContextInitialized,
+  setCircleResolution,
+  setContext,
+  setFramebuffer,
+  setViewport,
+  Shader,
+  getRandomColor,
+
+  getColor,
+  setColor,
+  getAlpha,
+
+  beginShape,
+  endShape,
+  vertex,
+
+  pushMatrix,
+  popMatrix,
+  translate,
+  scale,
+  rotate,
+
+  TRIANGLE_STRIP,
+  TRIANGLES,
+  FLOAT,
+}
