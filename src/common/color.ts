f5fcd2d3ed8336675dbe4d790e26361448d7f776
Oleg Pustovit <crystalboxesgfx@gmail.com>
2020-07-10 15:37:54 +0300

    current color state

--- src/common/color.ts
+++ src/common/color.ts
@@ -1,6 +1,7 @@
+
 export type Color = number | [number, number, number, number]
 
-export const getRgba = (col: Color) => {
+export const getRgba = (col: Color): [number, number, number, number] => {
   if (typeof (col) === 'number') {
     return [col, col, col, 1] as [number, number, number, number]
   } else {
@@ -15,3 +16,8 @@ export const getRandomColor = (): Color => {
     Math.random(),
     1]
 }
+
+let _currentColor: [number, number, number, number] = [0, 0, 0, 0]
+export function getColor() { return _currentColor }
+export function setColor(color: Color) { _currentColor = getRgba(color) }
+export function getAlpha() { return _currentColor[3] }
